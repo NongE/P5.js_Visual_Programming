090ab875b6edad448bd196a31f9c77f2570eea39
NongE <hsb9504@gmail.com>
2019-05-23 13:18:35 +0900

    VP 기말 프로젝트 초안

---
+++ Final_vp/sketch.js
@@ -0,0 +1,31 @@
+let bg;
+let train;
+let house;
+let y = -250;
+let x = 900;
+function setup() {
+  createCanvas(900, 500);
+  bg = loadImage('background.png');
+  train = loadImage('train.png');
+  house = loadImage('house.png');
+}
+
+function draw() {
+  background(bg);
+
+  image(house,0,y);
+  image(train,x,-250,0,0);
+
+  if(mouseIsPressed)
+  {
+    if(y < 0)
+    {
+      y+=5;
+    }
+    if(y >= 0 && x>-50)
+    {
+      x -=10;
+    }
+    print(mouseX, mouseY);
+  }
+}
