bf70f2c72607376c074b47f0300fb07ca26acf32
NongE <hsb9504@gmail.com>
2019-05-24 12:23:19 +0900

    성보: createImg -> loadImage 변경 , house down시 canvas out 설정, bg img 변경, line pade out 설정

--- Final_vp/sketch.js
+++ Final_vp/sketch.js
@@ -1,37 +1,56 @@
 let bg;
-let train;
 let house;
-let y = 0;
-let x = 1500;
-function setup() {
-  bg = createCanvas(1200, 800);
-  bg = createImg('background.png');
-  train = createImg('train_4.png');
-  house = createImg('house.png');
+let hx = 0,
+  hy = 0;
+let line4;
+let testBtn; // 버튼 위치 확인을 위해 임시로 넣은 이미지
+let houseFlag = 0;
 
+function setup() {
+  frameRate(60);
+  createCanvas(1200, 800);
+  bg = loadImage('background.png');
+  house = loadImage('house.png');
+  line4 = loadImage('line4.png');
+  testBtn = loadImage('testBtn.png');
+  tempBtn = createImg('tempBtn.png');
 }
 
 function draw() {
 
-  background(bg);// 여기는 배경화면
+  tint(255, 255, 255, 255);
+  background(bg);
 
-  // 이미지 위치조정
-  bg.position(0,0);
-  house.position(0,y);
-  train.position(x,-200);
+  image(house, hx, hy);
+  image(testBtn, 0, 30); // 버튼 레이아웃 확인을 위해 넣어둔 임시 이미지 / 삭제 필
 
-  image(train,x,-250,0,0);
-  //  이건 기차위치
-  if(mouseIsPressed) // 마우스 이벤트
-  {
-    if(y < -500)
-    {
-      y+=5;
+  if (houseFlag == 1) {
+    tint(255, 255, 255, 255 - (hy+5));
+    // 하우스 플래그 작동 시 4호선 투명도 조정
+    image(line4, 100, 110, 1200 * 0.85, 800 * 0.85);
+  } else {
+    if (hy != 500) {
+      //하단에 위치한 집이 안쪽에 위치해야 4호선 노출
+      image(line4, 100, 110, 1200 * 0.85, 800 * 0.85);
     }
-    if(y >= 0 && x>-50)
-    {
-      x -=10;
-    }
-    print(mouseX, mouseY);
   }
+  tint(255, 255, 255, 255);
+  // houseFlag로 인해 변경된 틴트값 변환
+
+
+  if (mouseIsPressed) {
+    if ((mouseX > 430 && mouseX < 445) && (mouseY > 440 && mouseY < 460)) {
+      houseFlag = 1;// 과천 클릭시
+    }
+
+  }
+
+
+  if (houseFlag == 1) {
+    hy += 10;
+    if (hy == 500) {
+      houseFlag = 0;
+    }
+  }
+
 }
