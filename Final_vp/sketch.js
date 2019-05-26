d65df765f37ce809baad901d66c1db8f38a62022
kmr2019 <kmr2019@naver.com>
2019-05-26 17:40:51 +0900

    정원 : 주석다는중

--- Final_vp/sketch.js
+++ Final_vp/sketch.js
@@ -1,29 +1,30 @@
-let bg;
-let house;
-let bridge;
-let hy = 555;
-let trainSpeed=0;
-let by = 0;
-let lineAreaX = 30;
-let lineTintCount = 0;
-let houseDownFlag = 0;
-let bridgeUpFlag = 0;
-let train;
-let line = [];
-let lineNumBtn = [];
-let lineNumBtnFlag = [];
+let bg;                   // 배경 변수
+let house;                // 집 변수
+let bridge;               // 다리 변수
+let train;                // 기차 이미지
+
+let hy = 555;             // 집 이랑 다리 이동을 위한 변수 y값
+let trainSpeed=0;         // 기차 들어오는 속도
+let lineAreaX = 30;       // 호선 버튼 스크롤 x 값
+let lineTintCount = 0;    // 호선 이미지 투명도
+let houseDownFlag = 0;    // 집 이미지 관련 불 변수
+let bridgeUpFlag = 0;     // 다리 이미지 관련 불 변수
+
+let line = [];            // 노선도 배열
+let lineNumBtn = [];      // 호선 버튼 배열
+let lineNumBtnFlag = [];  // 호선 선택 여부 버튼
 
 function setup() {
 
-  createCanvas(1200, 800);
-  bg = loadImage('background_4.png');
-  house = loadImage('house_2.png');
-  bridge = loadImage('bridge.png');
-  train = loadImage('train_2.png');
-  line[3] = loadImage('line4_2.png');
-  line[6] = loadImage('line7_2.png');
+  createCanvas(1200, 800);                                  // 캔버스 크기 설정
+  bg = loadImage('background.png');                       // 배경 이미지 로드
+  house = loadImage('house_2.png');                         // 집 이미지 로드
+  bridge = loadImage('bridge.png');                         // 다리 이미지 로드
+  train = loadImage('train_2.png');                         // 기차 이미지 로드
+  line[3] = loadImage('line4_2.png');                       // 4호선 이미지 로드
+  line[6] = loadImage('line7_2.png');                       // 7호선 이미지 로드
 
-  lineNumBtn[0] = createImg('lineNumBtn/line1Btn.png');
+  lineNumBtn[0] = createImg('lineNumBtn/line1Btn.png');     // 호선 버튼 로드
   lineNumBtn[1] = createImg('lineNumBtn/line2Btn.png');
   lineNumBtn[2] = createImg('lineNumBtn/line3Btn.png');
   lineNumBtn[3] = createImg('lineNumBtn/line4Btn.png');
@@ -34,31 +35,27 @@ function setup() {
   lineNumBtn[8] = createImg('lineNumBtn/line9Btn.png');
   lineNumBtn[9] = createImg('lineNumBtn/lineBunDangBtn.png');
   lineNumBtn[10] = createImg('lineNumBtn/lineGyeongUi_JungAngBtn.png');
-
 }
 
 function draw() {
+  background(bg);                                           // 배경 설정
+  image(house,0, hy);                                       // 집 출력
 
-  //tint(255, 255, 255, 255);
-  background(bg);
-  image(house, lineAreaX-600, hy);
-
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {                            // 버튼 이미지 출력
     lineNumBtn[i].position(lineAreaX + (i * 150), 0);
     lineNumBtn[i].size(80, 80);
   }
 
-  //tint(255, 255, 255, 0);
-
+ // 3호선 관련
   lineNumBtn[3].mousePressed(function() {
-    hy = 555;
-    trainSpeed = 0;
-    lineTintCount = 0;
-    lineNumBtnFlag[3] = 1;
-    lineNumBtnFlag[6] = 0;
+    hy = 555;                                             // 집이랑 다리 y값 초기화
+    trainSpeed = 0;                                       // 기차 위치 초기화
+    lineTintCount = 0;                                    // 노선도 투명도 초기화
+    lineNumBtnFlag[3] = 1;                                // 4호선 불 변수 참값으로 변경
+    lineNumBtnFlag[6] = 0;                                // 7호선 변수 참값으로 변경
   });
 
-  if (lineNumBtnFlag[3] == 1) {
+  if (lineNumBtnFlag[3] == 1) {                           //
     if (houseDownFlag == 1) {
       tint(255, 255 - (hy / 3));
       // 하우스 플래그 작동 시 4호선 투명도 조정
@@ -74,6 +71,7 @@ function draw() {
   }
 
 
+ // 7호선 관련
   lineNumBtn[6].mousePressed(function() {
     hy = 555;
     trainSpeed = 0;
@@ -125,8 +123,6 @@ function draw() {
     }
   }
 }
-
-
 
 function mouseWheel(event) {
   //move the square according to the vertical scroll amount
