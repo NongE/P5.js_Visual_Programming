caa38401bf93100bea4c7da7318c890544850990
kmr2019 <kmr2019@naver.com>
2019-05-27 16:32:47 +0900

    no message

--- Final_vp/sketch.js
+++ Final_vp/sketch.js
@@ -1,30 +1,54 @@
-let bg;                   // 배경 변수
-let house;                // 집 변수
-let bridge;               // 다리 변수
-let train;                // 기차 이미지
+let bg; // 배경 변수
+let house; // 집 변수
+let bridge; // 다리 변수
+let train; // 기차 변수
+let moon; // 달 변수
+let star;
+let total_people = 0;
 
-let hy = 555;             // 집 이랑 다리 이동을 위한 변수 y값
-let trainSpeed=0;         // 기차 들어오는 속도
-let lineAreaX = 30;       // 호선 버튼 스크롤 x 값
-let lineTintCount = 0;    // 호선 이미지 투명도
-let houseDownFlag = 0;    // 집 이미지 관련 불 변수
-let bridgeUpFlag = 0;     // 다리 이미지 관련 불 변수
+let star_position_x = [];
+let star_position_y = [];
 
-let line = [];            // 노선도 배열
-let lineNumBtn = [];      // 호선 버튼 배열
-let lineNumBtnFlag = [];  // 호선 선택 여부 버튼
+let house_position_y = 555; //집 초기 좌표
+let bridge_position_y = 555; // 다리 초기 좌표
+let train_position_x = 1200; // 기차 초기 좌표
+let moon_position_y = 800;
+
+let lineAreaX = 30; // 호선 버튼 스크롤 x 값
+let lineTintCount = 0; // 호선 이미지 투명도
+
+let houseDownFlag = 0; // 집 이미지 관련 불 변수
+let bridgeUpFlag = 0; // 다리 이미지 관련 불 변수
+let moon_fadein_Flag = 0; // 다리 이미지 관련 불 변수
+let star_fadein_Flag = 0; // 다리 이미지 관련 불 변수
+let station_Click_Flag = 0; // 호선을 클릭 했을 때 한번만 값 초기화 해주기 위한 변수
+
+let line = []; // 노선도 배열
+let lineNumBtn = []; // 호선 버튼 배열
+let lineNumBtnFlag = []; // 호선 선택 여부 버튼
 
 function setup() {
+  createCanvas(1200, 800); // 캔버스 크기 설정
 
-  createCanvas(1200, 800);                                  // 캔버스 크기 설정
-  bg = loadImage('background.png');                       // 배경 이미지 로드
-  house = loadImage('house_2.png');                         // 집 이미지 로드
-  bridge = loadImage('bridge.png');                         // 다리 이미지 로드
-  train = loadImage('train_2.png');                         // 기차 이미지 로드
-  line[3] = loadImage('line4_2.png');                       // 4호선 이미지 로드
-  line[6] = loadImage('line7_2.png');                       // 7호선 이미지 로드
+  star = loadImage('star.png');
+  bg = loadImage('background.png'); // 배경 이미지 로드
+  house = loadImage('house_2.png'); // 집 이미지 로드
+  bridge = loadImage('bridge.png'); // 다리 이미지 로드
+  train = loadImage('train_2.png'); // 기차 이미지 로드
+  moon = loadImage('moon.png');
 
-  lineNumBtn[0] = createImg('lineNumBtn/line1Btn.png');     // 호선 버튼 로드
+  line[0] = loadImage('line4_2.png');
+  line[1] = loadImage('line4_2.png');
+  line[2] = loadImage('line4_2.png');
+  line[3] = loadImage('line4_2.png'); // 4호선 이미지 로드
+  line[4] = loadImage('line7_2.png');
+  line[5] = loadImage('line7_2.png'); // 7호선 이미지 로드
+  line[6] = loadImage('line7_2.png'); // 7호선 이미지 로드
+  line[7] = loadImage('line7_2.png'); // 7호선 이미지 로드
+  line[8] = loadImage('line7_2.png'); // 7호선 이미지 로드
+  line[9] = loadImage('line7_2.png'); // 7호선 이미지 로드
+
+  lineNumBtn[0] = createImg('lineNumBtn/line1Btn.png'); // 호선 버튼 로드
   lineNumBtn[1] = createImg('lineNumBtn/line2Btn.png');
   lineNumBtn[2] = createImg('lineNumBtn/line3Btn.png');
   lineNumBtn[3] = createImg('lineNumBtn/line4Btn.png');
@@ -38,102 +62,115 @@ function setup() {
 }
 
 function draw() {
-  background(bg);                                           // 배경 설정
-  image(house,0, hy);                                       // 집 출력
+  background(bg); // 배경 설정
+  image(house, 0, house_position_y); // 집 출력
 
-  for (let i = 0; i < 10; i++) {                            // 버튼 이미지 출력
+  for (let i = 0; i < 10; i++) { // 버튼 이미지 출력
     lineNumBtn[i].position(lineAreaX + (i * 150), 0);
     lineNumBtn[i].size(80, 80);
   }
 
- // 3호선 관련
-  lineNumBtn[3].mousePressed(function() {
-    hy = 555;                                             // 집이랑 다리 y값 초기화
-    trainSpeed = 0;                                       // 기차 위치 초기화
-    lineTintCount = 0;                                    // 노선도 투명도 초기화
-    lineNumBtnFlag[3] = 1;                                // 4호선 불 변수 참값으로 변경
-    lineNumBtnFlag[6] = 0;                                // 7호선 변수 참값으로 변경
-  });
+  for (let i = 0; i < 10; i++) { // 호선의 갯수 만큼 포문 돈다. ex) 1호선 ~ 분당선
+    lineNumBtn[i].mousePressed(function() { // 해당 호선 버튼 클릭시
+      house_position_y = 555; // 집 위치 초기화
+      bridge_position_y = 555; // 다리 위치 초기화
+      moon_position_y = 800;
+      train_position_x = 1200; // 기차 위치 초기화
 
-  if (lineNumBtnFlag[3] == 1) {                           //
-    if (houseDownFlag == 1) {
-      tint(255, 255 - (hy / 3));
-      // 하우스 플래그 작동 시 4호선 투명도 조정
-      image(line[3], 100, 110, 1200 * 0.85, 800 * 0.85);
-    } else {
-      if (hy == 555) {
-        if (lineTintCount < 255) lineTintCount += 20;
-        //하단에 위치한 집이 안쪽에 위치해야 4호선 노출
-        tint(255, 255, 255, lineTintCount);
-        image(line[3], 100, 110, 1200 * 0.85, 800 * 0.85);
+      star_fadein_Flag = 0;
+      moon_fadein_Flag = 0;
+      bridgeUpFlag = 0; // 기차 불변수 초기화
+      lineTintCount = 0; // 노선도 투명도 초기화
+
+      for (let j = 0; j < 10; j++) // 호선 갯수 만큼 포문 돈다.
+        lineNumBtnFlag[j] = 0; // 일단 모든 호선 Flag값 false로 초기화 하고
+
+      lineNumBtnFlag[i] = 1; // 클릭한 호선만 true로 변환
+    });
+
+    if (lineNumBtnFlag[i] == 1) { // 해당 호선 버튼을 클릭 했을 경우
+      if (houseDownFlag == 1) { // 만약에 houseDownFlag값이 1일 경우
+        tint(255, 255 - (house_position_y / 3)); // 하우스 플래그 작동 시 노선도 투명도 조정 어둡게
+        image(line[i], 100, 110, 1200 * 0.85, 800 * 0.85); // 노선도 이미지 출력
+      } else {
+        if (house_position_y == 555) { // hy값이 555일 경우
+          if (lineTintCount < 255) // 노선도 점점 밝게 해줌.
+            lineTintCount += 20;
+
+          tint(255, 255, 255, lineTintCount); // 노선도 점점 밝게 해줌.
+          image(line[i], 100, 110, 1200 * 0.85, 800 * 0.85); // 노선도 이미지 출력
+        }
       }
     }
   }
 
+  tint(255, 255); // houseDownFlag로 인해 변경된 틴트값 변환
 
- // 7호선 관련
-  lineNumBtn[6].mousePressed(function() {
-    hy = 555;
-    trainSpeed = 0;
-    lineTintCount = 0;
-    lineNumBtnFlag[3] = 0;
-    lineNumBtnFlag[6] = 1;
-  });
+  if (houseDownFlag == 0 && mouseIsPressed) { // 기차가 들어오기 전 이면서 마우스 클릭했을 경우(아직 호선 선택 이전)
+    if (lineNumBtnFlag[3] == 1) { // 4호선 버튼을 선택 했고
+      if ((mouseX > 420 && mouseX < 445) && (mouseY > 440 && mouseY < 460)) // 과천 클릭 시
+        station_Click_Flag = 1; // 역을 클릭했다는 정보 관련 변수 참으로 변환
+    } else if (lineNumBtnFlag[6] == 1) { // 7호선 버튼을 선택 했고
+    }
 
-  if (lineNumBtnFlag[6] == 1) {
-    if (houseDownFlag == 1) {
-      tint(255, 255 - (hy / 3));
-      // 하우스 플래그 작동 시 4호선 투명도 조정
-      image(line[6], 100, 110, 1200 * 0.85, 800 * 0.85);
-    } else {
-      if (hy == 555) {
-        if (lineTintCount < 255) lineTintCount += 20;
-        //하단에 위치한 집이 안쪽에 위치해야 4호선 노출
-        tint(255, lineTintCount);
-        image(line[6], 100, 110, 1200 * 0.85, 800 * 0.85);
+    if (station_Click_Flag == 1) { // 역을 선택 했으니 이제 집 이미지 없애고 기차 들어오게 해야하니 그걸 처리해주는 부분
+
+      for (let i = 0; i < 10; i++) {
+        star_position_x[i] = random(100, 700);
+        star_position_y[i] = random(100, 500);
       }
+
+      houseDownFlag = 1; //houseDownFlag 값 참으로 변환 집 이미지 없애야 하므로
+      bridgeUpFlag = 1; // 다리 이미지 참으로 변환
+      station_Click_Flag = 0; // 값 한번 초기화 했으니 다시 station_Click_Flag false로
     }
   }
-  //print(hy);
-  tint(255, 255);
-  // houseDownFlag로 인해 변경된 틴트값 변환
 
-  if (mouseIsPressed) {
+  if (moon_fadein_Flag == 1) {
+    if (moon_position_y >= 100)
+      moon_position_y -= 45;
 
-    if ((mouseX > 420 && mouseX < 445) && (mouseY > 440 && mouseY < 460)) {
-      houseDownFlag = 1; // 과천 클릭시
-      bridgeUpFlag = 1;
+    else {
+      star_fadein_Flag = 1;
     }
 
-
+    image(moon, 900, moon_position_y);
   }
 
-  if (bridgeUpFlag == 1) {
-
-    image(bridge, 0, 1950 - hy);
-    image(train, 1500 - trainSpeed, 560);
-
+  if (star_fadein_Flag == 1) {
+    for (let i = 0; i < 10; i++)
+      image(star, star_position_x[i], star_position_y[i]);
   }
 
-  if (houseDownFlag == 1) {
-    trainSpeed+=38;
-    hy += 25;
-    if (hy >= 1500) {
-      houseDownFlag = 0;
+  if (bridgeUpFlag == 1) { // bridgeUpFlag 변수가 참값일 경우
+    if (bridge_position_y >= 455)
+      bridge_position_y -= 25;
+
+    if (train_position_x >= 70)
+      train_position_x -= 40;
+
+    else
+      moon_fadein_Flag = 1;
+
+    image(bridge, 0, bridge_position_y);
+    image(train, train_position_x, 555);
+  }
+
+
+
+  if (houseDownFlag == 1) { //houseDownFlag 값 침일 경우
+    house_position_y += 60; // 집을 점점 사라지게 하기 위해
+    if (house_position_y >= 800) { //  1500 이상일 경우
+      houseDownFlag = 0; //houseDownFlag다시 거짓으로
     }
   }
 }
 
 function mouseWheel(event) {
-  //move the square according to the vertical scroll amount
-  if ((mouseY >= 0 && mouseY <= 80)) {
+  if ((mouseY >= 0 && mouseY <= 70)) {
+    lineAreaX += event.delta;
 
-     lineAreaX += event.delta;
-    if (lineAreaX > 30) {
+    if (lineAreaX > 30)
       lineAreaX = 30;
-    }
-
   }
-  //uncomment to block page scrolling
-  //return false;
 }
